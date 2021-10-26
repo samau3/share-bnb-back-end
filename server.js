7aed59a532b20426362ff6fa093656facffea201
vcheng33 <vanessa.ah.cheng@gmail.com>
2021-10-26 12:12:35 -0700

    started writing s3 functions

---
+++ server.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const app = require("./app");
+const { PORT } = require("./config");
+const express = require("./express");
+
+app.listen(PORT, function () {
+  console.log(`Started on http://localhost:${PORT}`);
+});
