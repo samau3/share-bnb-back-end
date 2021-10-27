3a45e6dcd9c5b4790b6e74c7f8172a1280e3d75f
samau3 <samau415@gmail.com>
2021-10-26 17:10:28 -0700

    resolving req.file reading issue

--- routes/listings.js
+++ routes/listings.js
@@ -6,33 +6,34 @@ const { generateUploadUrl } = require("../s3");
 
 const router = new express.Router();
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public')
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname)
-    }
-})
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, 'public')
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, Date.now() + '-' + file.originalname)
+//     }
+// })
 
-const upload = multer({ storage: storage }).single('file')
+// const upload = multer({ storage: storage }).single('file')
+const upload = multer();
 
-router.post("/", async function (req, res, next) {
+router.post("/", upload.single('file'), async function (req, res, next) {
     // const validator = jsonschema.validate(req.body, companyNewSchema);
     // if (!validator.valid) {
     //   const errs = validator.errors.map(e => e.stack);
     //   throw new BadRequestError(errs);
     // }
-    upload(req, res, (err) => {
-        if (err) {
-            res.sendStatus(500);
-        }
-        res.send(req.file);
-        console.log("listings post route upload fn", req.file);
-    });
+    // upload(req, res, (err) => {
+    //     if (err) {
+    //         res.sendStatus(500);
+    //     }
+    //     res.send(req.file);
+    // });
 
+    console.log("listings post route upload fn", req.file);
 
-    // const upload = await generateUploadUrl(req.body)
+    // const upload = await generateUploadUrl(req.body);
     // console.log("listings post route", upload);
     // const listing = await Listing.create(req.body);
     // return res.status(201).json({ upload });
