b87cffbcc9735ebc1b29b412a13efaff1e4e0fae
samau3 <samau415@gmail.com>
2021-10-28 14:08:35 -0700

    resolved login issues

--- routes/users.js
+++ routes/users.js
@@ -63,8 +63,7 @@ router.get("/", ensureAdmin, async function (req, res, next) {
 
 /** GET /[username] => { user }
  *
- * Returns { username, firstName, lastName, isAdmin, jobs }
- *   where jobs is { id, title, companyHandle, companyName, state }
+ * Returns { username, firstName, lastName, isAdmin }
  *
  * Authorization required: admin or same user-as-:username
  **/
